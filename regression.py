20a18b023e42f36545efae6d143ff28b48d4bfed
David Becerra <dbecerra@college.harvard.edu>
2015-03-02 17:47:04 -0500

    Commented code

--- regression.py
+++ regression.py
@@ -1,9 +1,25 @@
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 
+# Returns a model fitted to (X,y) using Logistic Regression
 def LogRegression(X, y):
-  model = LogisticRegression(penalty="l2")
+  """
+  input: 
+    X is the design matrix
+    y = training output (i.e. t_train)
+  return: 
+    LogisticRegression object that is fitted to the data
+  """
+  model = LogisticRegression(penalty="l2") # using L2 norm penalty
   return model.fit(X, y)
 
+# Returns models prediction as list on the data X
 def makePrediction(model, X):
+  """
+  args:
+    model = a sklearn 'regression' object that is fitted to data
+    X = test data to make predictions on
+  return:
+    a numpy array of class assignments/predictions to each datum in X
+  """
   return model.predict(X)
